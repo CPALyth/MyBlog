6a7547630d19f3289ee6f669729f58aa3ba623b1
CPALyth <ws156858@163.com>
2021-12-14 14:37:18 +0800

    feat: 添加博客

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,21 +1,69 @@
 <template>
   <div class="add-blog">
-    <h2>Add-Blog</h2>
+    <h2>添加博客</h2>
+    <form action="">
+      <label>博客标题</label>
+      <input type="text" v-model="blog.title" required />
+      <label>博客内容</label>
+      <textarea v-model="blog.content"></textarea>
+
+      <div id="checkboxes">
+        <input type="checkbox" value="Vue.js" v-model="blog.categories" />
+        <label>Vue.js</label>
+
+        <input type="checkbox" value="Node.js" v-model="blog.categories" />
+        <label>Node.js</label>
+
+        <input type="checkbox" value="React.js" v-model="blog.categories" />
+        <label>React.js</label>
+
+        <input type="checkbox" value="Angular4" v-model="blog.categories" />
+        <label>Angular4</label>
+      </div>
+
+      <label>作者:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors" :key="author">
+          {{ author }}
+        </option>
+      </select>
+
+      <button @click.prevent="post">添加博客</button>
+    </form>
+    <hr />
+    <div id="preview">
+      <h3>博客总览</h3>
+      <p>博客标题: {{ blog.title }}</p>
+      <p>博客内容:</p>
+      <p>{{ blog.content }}</p>
+      <p>博客分类:</p>
+      <ul>
+        <li v-for="category in blog.categories" :key="category">
+          {{ category }}
+        </li>
+      </ul>
+      <p>作者: {{ blog.author }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'add-blog',
-  data () {
+  name: "add-blog",
+  data() {
     return {
-      
-    }
+      blog: {
+        title: "",
+        content: "",
+        categories: [],
+        author: "",
+      },
+      authors: ["Eward", "Tim", "Bob"],
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
