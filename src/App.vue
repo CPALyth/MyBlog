f3786624d992573c4347f9d085eab430c0435eb5
CPALyth <ws156858@163.com>
2021-12-15 15:43:36 +0800

    feat: 展示博客并请求数据

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <add-blog></add-blog>
+    <!-- <add-blog></add-blog> -->
+    <show-blog></show-blog>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog'
+import ShowBlog from './components/ShowBlog.vue'
 
 export default {
   name: 'App',
   components: {
-    AddBlog
+    AddBlog,
+    ShowBlog
   }
 }
 </script>
---
+++ src/components/ShowBlog.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="show-blog">
+    <h1>博客总览</h1>
+    <div v-for="blog in blogs" class="single-blog" :key=blog>
+      <h2>{{blog.title}}</h2>
+      <article>
+        {{blog.body}}
+      </article>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "show-blog",
+  data() {
+    return {};
+  },
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function (data) {
+        console.log(data)
+        this.blogs = data.body.slice(0, 10);
+        console.log(this.blogs)
+      });
+  },
+};
+</script>
+
+<style>
+#show-blog {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
